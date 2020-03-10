270d5b928e02997e7100af0be19a8774daffb4a0
Fabrício Soares <fabriciosoares@live.com>
2020-03-10 19:07:56 -0300

    Adding initial template.

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function Home() {
+    return (
+
+        <div className="container text-center">
+            <div className="row justify-content-center">
+                <div className="col-10 col-md-10 col-lg-8 col-xl-7">
+                    <div className="display-4 text-primary mt-3 mb-2">
+                        Meeting Log
+      </div>
+                    <p className="lead">
+                        This simple app creates meetings, allows people to check
+                        in, and picks random users to award giveaways.
+      </p>
+
+                    <a href="/register" className="btn btn-outline-primary mr-2">
+                        Register
+      </a>
+                    <a href="/login" className="btn btn-outline-primary mr-2">
+                        Log In
+      </a>
+                    <a href="/meetings" className="btn btn-primary">
+                        Meetings
+      </a>
+                </div> {/*columns*/}
+            </div>
+        </div>
+    )
+
+}
+
+export default Home
